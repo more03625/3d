574432f3096f69d67520d77da7c35b9681a80fff
Rahul Pandit More <more03625@gmail.com>
2020-01-15 19:01:46 +0530

    base_url update

--- footer.php
+++ footer.php
@@ -12,21 +12,22 @@
          <a class="top" id="top" href="#top"><i class="fa fa-angle-double-up" aria-hidden="true"></i> </a>
       </div>
       <canvas id="canvas1"></canvas>
-      <script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/jquery-min.js"></script>
-      <script src="assets/js/popper.min.js"></script>
-      <script src="assets/js/bootstrap.min.js"></script>
-      <script src="assets/js/all-plugins.js"></script>
-      <script src="assets/js/timeline.min.js"></script>
-      <script src='assets/js/wave/three.min.js'></script>
-      <script src='assets/js/wave/Projector.js'></script>
-      <script src='assets/js/wave/CanvasRenderer.js'></script>
-      <script src="assets/js/wave/index.js"></script>
-      <script src="assets/js/bubbly-bg.js"></script>
-      <script src="assets/js/amcharts/amcharts.js"></script>
-      <script src="assets/js/amcharts/serial.js"></script>
-      <script src="assets/js/amcharts/export.min.js"></script>
-      <script src="assets/js/amcharts/none.js"></script>
-      <script src="assets/js/custom.js"></script>
+      <script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+      <script src=<?=base_url()."assets/js/jquery-min.js"?>></script>
+      <script src=<?=base_url()."assets/js/popper.min.js"?>></script>
+      <script src=<?=base_url()."assets/js/bootstrap.min.js"?>></script>
+      <script src=<?=base_url()."assets/js/all-plugins.js"?>></script>
+      <script src=<?=base_url()."assets/js/timeline.min.js"?>></script>
+      <script src=<?=base_url()."assets/js/wave/three.min.js"?>></script>
+      <script src=<?=base_url()."assets/js/wave/Projector.js"?>></script>
+      <script src=<?=base_url()."assets/js/wave/CanvasRenderer.js"?>></script>
+      <script src=<?=base_url()."assets/js/wave/index.js"?>></script>
+      <script src=<?=base_url()."assets/js/bubbly-bg.js"?>></script>
+      <script src=<?=base_url()."assets/js/amcharts/amcharts.js"?>></script>
+      <script src=<?=base_url()."assets/js/amcharts/serial.js"?>></script>
+      <script src=<?=base_url()."assets/js/amcharts/export.min.js"?>></script>
+      <script src=<?=base_url()."assets/js/amcharts/none.js"?>></script>
+      <script src=<?=base_url()."assets/js/custom.js"?>></script>
    </body>
    <!-- Mirrored from iqonicthemes.com/themes/coinex/ico/index by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 Jan 2020 05:18:21 GMT -->
 </html>
