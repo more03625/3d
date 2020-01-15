953271e5f66e0656bff1626ad0d47032449f2e88
Rahul Pandit More <more03625@gmail.com>
2020-01-15 18:01:21 +0530

    seaprate

---
+++ footer.php
@@ -0,0 +1,32 @@
+<div class="copyright-box text-center iq-bg-over iq-over-black-20 text-center iq-ptb-20 iq-mt-100">
+            <div class="container-fluid">
+               <div class="row">
+                  <div class="col-sm-12">
+                     <div class="iq-copyright iq-font-white">© Copyright 2020 BWRL Developed by <a href="https://bharatwireropes.com/" target="_blank">Bharat Wire Ropes.</a></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <div id="back-to-top">
+         <a class="top" id="top" href="#top"><i class="fa fa-angle-double-up" aria-hidden="true"></i> </a>
+      </div>
+      <canvas id="canvas1"></canvas>
+      <script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/jquery-min.js"></script>
+      <script src="assets/js/popper.min.js"></script>
+      <script src="assets/js/bootstrap.min.js"></script>
+      <script src="assets/js/all-plugins.js"></script>
+      <script src="assets/js/timeline.min.js"></script>
+      <script src='assets/js/wave/three.min.js'></script>
+      <script src='assets/js/wave/Projector.js'></script>
+      <script src='assets/js/wave/CanvasRenderer.js'></script>
+      <script src="assets/js/wave/index.js"></script>
+      <script src="assets/js/bubbly-bg.js"></script>
+      <script src="assets/js/amcharts/amcharts.js"></script>
+      <script src="assets/js/amcharts/serial.js"></script>
+      <script src="assets/js/amcharts/export.min.js"></script>
+      <script src="assets/js/amcharts/none.js"></script>
+      <script src="assets/js/custom.js"></script>
+   </body>
+   <!-- Mirrored from iqonicthemes.com/themes/coinex/ico/index by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 Jan 2020 05:18:21 GMT -->
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>3d-Design</title>
+      <link rel="shortcut icon" href="assets/images/favicon.ico" />
+      <link href="https://fonts.googleapis.com/css?family=Comfortaa:300,400,700" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Montserrat:100i,200,300,400,700,800,900" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/owl-carousel/owl.carousel.css" />
+      <link rel="stylesheet" href="assets/css/magnific-popup/magnific-popup.css" />
+      <link rel="stylesheet" type="text/css" href="assets/css/animate.css" />
+      <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css" />
+      <link rel="stylesheet" type="text/css" href="assets/css/export.css" />
+      <link rel="stylesheet" type="text/css" href="assets/css/line-awesome.min.css" />
+      <link rel="stylesheet" type="text/css" href="assets/css/mediaelementplayer.min.css" />
+      <link rel="stylesheet" type="text/css" href="assets/css/timeline.min.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+   </head>
+   <body data-spy="scroll" data-offset="80">
