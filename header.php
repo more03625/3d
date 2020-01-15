e246867272d1fb08eceb6504334aaf4e1a35b07a
Rahul Pandit More <more03625@gmail.com>
2020-01-15 19:00:58 +0530

    base_url update

--- header.php
+++ header.php
@@ -7,17 +7,17 @@
       <link rel="shortcut icon" href="assets/images/favicon.ico" />
       <link href="https://fonts.googleapis.com/css?family=Comfortaa:300,400,700" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Montserrat:100i,200,300,400,700,800,900" rel="stylesheet">
-      <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-      <link rel="stylesheet" type="text/css" href="assets/css/owl-carousel/owl.carousel.css" />
-      <link rel="stylesheet" href="assets/css/magnific-popup/magnific-popup.css" />
-      <link rel="stylesheet" type="text/css" href="assets/css/animate.css" />
-      <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css" />
-      <link rel="stylesheet" type="text/css" href="assets/css/export.css" />
-      <link rel="stylesheet" type="text/css" href="assets/css/line-awesome.min.css" />
-      <link rel="stylesheet" type="text/css" href="assets/css/mediaelementplayer.min.css" />
-      <link rel="stylesheet" type="text/css" href="assets/css/timeline.min.css">
-      <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-      <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
-      <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+      <link rel="stylesheet" type="text/css" href=<?=base_url()."assets/css/bootstrap.min.css"?>>
+      <link rel="stylesheet" type="text/css" href=<?=base_url()."assets/css/owl-carousel/owl.carousel.css"?>>
+      <link rel="stylesheet" href=<?=base_url()."assets/css/magnific-popup/magnific-popup.css"?>>
+      <link rel="stylesheet" type="text/css" href=<?=base_url()."assets/css/animate.css"?>>
+      <link rel="stylesheet" type="text/css" href=<?=base_url()."assets/css/font-awesome.min.css"?>>
+      <link rel="stylesheet" type="text/css" href=<?=base_url()."assets/css/export.css"?>>
+      <link rel="stylesheet" type="text/css" href=<?=base_url()."assets/css/line-awesome.min.css"?>>
+      <link rel="stylesheet" type="text/css" href=<?=base_url()."assets/css/mediaelementplayer.min.css"?>>
+      <link rel="stylesheet" type="text/css" href=<?=base_url()."assets/css/timeline.min.css"?>>
+      <link rel="stylesheet" type="text/css" href=<?=base_url()."assets/css/style.css"?>>
+      <link rel="stylesheet" type="text/css" href=<?=base_url()."assets/css/responsive.css"?>>
+      <link rel="stylesheet" type="text/css" href=<?=base_url()."assets/css/custom.css"?>>
    </head>
    <body data-spy="scroll" data-offset="80">
